f01024fd3994ade4b0f31766e60523d6c556a050
Johan Higuita <higuitajohan@gmail.com>
2019-02-24 22:23:18 -0500

    Passed testing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { text: "" };
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" value={this.state.text} onChange = {this.updateText.bind(this)} />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
+  }
+
+  updateText(evt){
+    this.setState({
+      text: evt.target.value
+    });
   }
 }
 
